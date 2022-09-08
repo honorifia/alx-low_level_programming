8a8102cde8f729d15e3a0d68ed80f8d000ef7cb4
honorifica <mkajiwonele@must.ac.mw>
2022-09-08 16:58:45 -0700

    4-puts.c a C program that prints exactly with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,34 @@
+#include <stdio.h>                                                                                 
+
+                                                                                                   
+
+                                                                                                   
+
+                                                                                                 
+
+/**                                                                                                
+ *
+ *  *                                                                                                 
+ *
+ *   *  * main - Entry point                                                                           
+ *
+ *    *                                                                                                 
+ *
+ *     *   *                                                                                             
+ *
+ *      *                                                                                                 
+ *
+ *       *    * Return: Always 0 (Success)                                                                 
+ *
+ *        *                                                                                                 
+ *
+ *         *     */                                                                                          
+
+                                                                                                  
+int main(void)                                                                                
+{                                                                                                  
+
+	        char str1[] = "Programming is like building a multilingual puzzle\n";        
+		                puts(str1);                                                  
+				            return (0);                                            
+}
--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,4 +1,4 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 
 
@@ -15,7 +15,11 @@ nclude <stdio.h>
 int main(void)
 
 {
+	char str1[] = "Programming is like building a multilingual puzzle\n";
+
+		puts(str1);
 
 	    return (0);
+
 
 }
