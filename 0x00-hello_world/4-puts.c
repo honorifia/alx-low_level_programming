8cabe93dd64b8821ef05500a76227ccb036317b3
honorifica <mkajiwonele@must.ac.mw>
2022-09-08 17:17:35 -0700

    4-puts.c a C program that prints exactly with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-/*
+/**
+ * main -entry input
  * enter string
  * print with puts
  * always return(0)
  */
-                                                                                                  
-int main(void)                                                                                
-{                                                                                                  
-
-	        char str1[]="\"Programming is like building a multilingual puzzle";        
-		 puts(str1);                                    
-		 return (0);                                            
+int main(void)
+{
+	char str1[]="\"Programming is like building a multilingual puzzle";
+	puts(str1);
+	return (0);
 }
