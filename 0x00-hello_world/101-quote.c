b3fdd47b8e7a7d2b2796c71ac4b8ddbfa2645015
honorifica <mkajiwonele@must.ac.mw>
2022-09-08 17:57:24 -0700

    101-quote.c a script that generates the assembly code (Intel syntax) of a C code and save it in an output file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main -entry
+ *
+ * Return: success (1) return
+ */
+int main(void)
+{
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(str1);
+	return (1);
+}
