2786f3450e423ecc8965c3d2486d036aca01a7e3
honorifica <mkajiwonele@must.ac.mw>
2022-09-08 17:05:39 -0700

    4-puts.c a C program that prints exactly with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,34 +1,15 @@
-#include <stdio.h>                                                                                 
+#include <stdio.h>
 
-                                                                                                   
-
-                                                                                                   
-
-                                                                                                 
-
-/**                                                                                                
- *
- *  *                                                                                                 
- *
- *   *  * main - Entry point                                                                           
- *
- *    *                                                                                                 
- *
- *     *   *                                                                                             
- *
- *      *                                                                                                 
- *
- *       *    * Return: Always 0 (Success)                                                                 
- *
- *        *                                                                                                 
- *
- *         *     */                                                                                          
-
+/*
+ * enter string
+ * print with puts
+ * always return(0)
+ */
                                                                                                   
 int main(void)                                                                                
 {                                                                                                  
 
-	        char str1[] = "Programming is like building a multilingual puzzle\n";        
-		                puts(str1);                                                  
-				            return (0);                                            
+	        char str1[]="\"Programming is like building a multilingual puzzle\n";        
+		 puts(str1);                                    
+		 return (0);                                            
 }
