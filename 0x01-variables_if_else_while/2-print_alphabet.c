6a736de104e12972b43293e34018a7dee8f4ae3a
honorifica <mkajiwonele@must.ac.mw>
2022-09-09 17:30:01 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry
+ * code
+ * Return: return 0 success
+ */
+int main(void)
+{
+	char alpha;
+
+	alpha = 'a';
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -6,3 +6,5 @@
 
 1-last_digit.c
 
+2-print_alphabet.c
+
