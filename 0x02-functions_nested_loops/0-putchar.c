a1e1ceca2424a649207ba5429df1780df3f1f755
honorifica <mkajiwonele@must.ac.mw>
2022-09-13 05:58:36 -0700

    0-putchar.c print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+/**
+ * main - Main function
+ *
+ * Return: Return 0 success
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
