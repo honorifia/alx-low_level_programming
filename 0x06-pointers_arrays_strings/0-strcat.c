fd69563ee8e14a16933820084d13117556e6a6ff
honorifica <mkajiwonele@must.ac.mw>
2022-09-23 06:15:05 -0700

    a function that copies a string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,2 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
+/**
+ * _strcat - Concatenates two strings
+ * @dest: the destination string
+ * @src: the source string
+ * Return: The source string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = 0, i;
+
+	while (dest[dest_len])
+		dest_len++;
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dest_len] = src[i];
+		dest_len++;
+	}
+
+	dest[dest_len] = '\0';
+	return (dest);
+}
