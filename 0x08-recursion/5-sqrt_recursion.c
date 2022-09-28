636141bf785f9d1f922846b8ccbaee6a3f02ebb3
honorifica <mkajiwonele@must.ac.mw>
2022-09-28 16:32:05 -0700

    Final changes to task 3 & 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -28,8 +28,11 @@ int _sqrt(int n, int i)
 {
 	int sqrt = i * i;
 
+	if (sqrt > n)
+		return (-1);
+
 	if (sqrt == n)
 		return (i);
-	else
-		return (_sqrt(n, i + 1));
+
+	return (_sqrt(n, i + 1));
 }
