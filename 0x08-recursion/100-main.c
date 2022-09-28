2c6a1dd252f40b42171e125663553a080b3e977f
honorifica <mkajiwonele@must.ac.mw>
2022-09-28 15:48:06 -0700

    a function that returns 1 if a string is a palindrome and 0 if not.

---
+++ 0x08-recursion/100-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/** main - check the code
+ *Return: 0
+ */
+
+int main(void)
+{
+	int r;
+
+	r = is_palindrome("level");
+	printf("%d\n", r);
+	r = is_palindrome("abcdecba");
+	printf("%d\n", r);
+	return (0);
+}
